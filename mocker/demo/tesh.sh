d1e6d2752e2108d068d8aca892ef478ff9d9558f
dinhanhhuy <huyda@vng.com.vn>
2023-02-18 02:37:27 +0700

    feat: update demo

--- mocker/demo/tesh.sh
+++ mocker/demo/tesh.sh
@@ -1,38 +1,47 @@
 #!/bin/bash
 set -e
 
-echo "read Mockerfile" | cowsay
+echo "read Mockerfile
+
+cat golang1.Mockerfile" | cowsay
 cat golang1.Mockerfile
 
 # image name need to be short
-echo "build image `go1` from Mockerfile" | cowsay
+echo "build image `go1` from Mockerfile
+
+mocker build img1 golang1.Mockerfile" | cowsay
 mocker build img1 golang1.Mockerfile
 
-echo "list all images" | cowsay
+echo "list all images
+
+mocker images" | cowsay
 mocker images
 
 # container name need to be short
-echo "run new container" | cowsay
-mocker run con1 img1
+echo "run new container
+
+mocker run con3 img1" | cowsay
+mocker run con3 img1
 
 echo "list container" | cowsay
 mocker ps
 
-IP=$(mocker ps | grep con1 | awk '{print $NF}')
+IP=$(mocker ps | grep con3 | awk '{print $NF}')
 PORT=10000
-echo "container con1 running at $IP:$PORT\n
-send request to container\n
+echo "container con3 running at $IP:$PORT - 
 curl $IP:$PORT" | cowsay
 curl $IP:$PORT
 
-echo "check if host have port $PORT\n
+echo "check if host have port $PORT - 
 netstat -lnpt | grep $PORT" | cowsay
 netstat -lnpt | grep $PORT
 
-echo "execute to pod and check for network\n
-mocker exec con1" | cowsay
-mocker exec con1
+echo "execute to pod and check for network - 
+mocker exec con3" | cowsay
+mocker exec con3
 
-echo "check network inside container\n
+echo "check network inside container - 
 netstat -lnpt | grep $PORT" | cowsay
 netstat -lnpt | grep $PORT
+
+echo "demo success"
