e51290c2077ab824b3905f540cd05ec5f0637d79
dinhanhhuy <huyda@vng.com.vn>
2023-02-17 17:23:19 +0700

    feat: update install script

--- mocker/install.sh
+++ mocker/install.sh
@@ -4,10 +4,8 @@ set -e
 
 echo "Start installing mocker"
 yum install git -y
-yum install tr -y
-yum install diff -y
 yum install cowsay -y
-yum install base64 -y
-git clone https://github.com/dinhanhhuy/mocker-k1s.git .
+yum install coreutils -y # base64
+git clone https://github.com/dinhanhhuy/mocker-k1s.git
 cd mocker-k1s/mocker
 ./network.sh init
